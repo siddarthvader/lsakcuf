38c69e5bd7f6630738c37bbd124a40e17ee770fc
siddharth <siddharthjn93@gmail.com>
2017-02-16 10:57:41 +0530

    react changes

--- react/cooked/script.js
+++ react/cooked/script.js
@@ -1,18 +1,37 @@
+
+function getGreeting(user) {
+  return React.createElement(
+    "h1",
+    null,
+    " hello, ",
+    formatName(user),
+    "  "
+  );
+}
+
 function formatName(user) {
-  return user.firstName + ' ' + user.lastName;
+  return user.firstName + ' my name is ' + user.lastName;
 }
 
 const user = {
-  firstName: 'Harper',
-  lastName: 'Perez'
+  firstName: "Sid",
+  lastName: "Jain"
 };
 
 const element = React.createElement(
-  'h1',
+  "div",
   null,
-  'Hello, ',
-  formatName(user),
-  '!'
+  React.createElement(
+    "h1",
+    null,
+    " This is growing "
+  ),
+  React.createElement(
+    "p",
+    null,
+    "react is new ",
+    getGreeting(user)
+  )
 );
 
 ReactDOM.render(element, document.getElementById('root'));
